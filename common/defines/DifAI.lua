ef673dadbab0fe989982a35ea53aca24bb2f0f32
Spirit <34450337+SpiritBaker@users.noreply.github.com>
2022-01-17 17:58:15 +0600

    adding Ai defines

---
+++ common/defines/DifAI.lua
@@ -0,0 +1,11 @@
+NDefines.NAI.ARTILLERY_FRACTION = 0.2
+NDefines.NAI.FORCE_COMPOSITION_CHANGE_TECH_LEVEL = 16
+NDefines.NAI.NAVY_BUDGET_FRACTION = 0.4 -- AI will spend a maximum of this fraction of monthly income on navy maintenance (based off wartime costs)
+NDefines.NAI.TRANSPORT_FRACTION = 0.35
+NDefines.NAI.BIGSHIP_FRACTION = 0.4
+NDefines.NAI.CALL_IN_ALLIES_POWER_RATIO = 2.5  --AI will only call in allies in an offensive war if their military power ratio to the enemy is less than this.
+NDefines.NAI.DEBASE_THRESHOLD = -5000 -- AI will not debase if it has more gold than this.
+NDefines.NAI.ADVISOR_BUDGET_FRACTION = 1 -- AI will spend a maximum of this fraction of monthly income on advisor maintenance
+
+NDefines.NAI.ARMY_DISTANCE_SCORE_IMPACT	= 1	--Army <-> province distance impact on province evaluation / divides total score by this amount
+NDefines.NAI.BORDER_DISTANCE_SCORE_IMPACT =	20 --Border <-> province distance impact on province evaluation / actual distance penalty
