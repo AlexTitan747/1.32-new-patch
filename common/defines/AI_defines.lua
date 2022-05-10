c95c09ad587490ece699b5337fd24f5b167433de
Spirit <34450337+SpiritBaker@users.noreply.github.com>
2022-05-10 17:03:01 +0600

    adjusting Logic for picking ideas

--- common/defines/AI_defines.lua
+++ common/defines/AI_defines.lua
@@ -6,9 +6,10 @@ NDefines.NAI.BIGSHIP_FRACTION = 0.4
 NDefines.NAI.CALL_IN_ALLIES_POWER_RATIO = 2.5  --AI will only call in allies in an offensive war if their military power ratio to the enemy is less than this.
 NDefines.NAI.DEBASE_THRESHOLD = -5000 -- AI will not debase if it has more gold than this.
 NDefines.NAI.ADVISOR_BUDGET_FRACTION = 1 -- AI will spend a maximum of this fraction of monthly income on advisor maintenance
-NDefines.NAI.ARMY_BUDGET_FRACTION = 0.85
+NDefines.NAI.ARMY_BUDGET_FRACTION = 0.9
 NDefines.NAI.MAX_CAV_PERCENTAGE = 100
 NDefines.NAI.MIN_CAV_PERCENTAGE = 0
+NDefines.NAI.OVER_FORCELIMIT_AVOIDANCE_FACTOR = 2 --The higher this number is the less willing the AI will be to exceed forcelimits.
 
 NDefines.NAI.ARMY_DISTANCE_SCORE_IMPACT	= 1	--Army <-> province distance impact on province evaluation / divides total score by this amount
 NDefines.NAI.BORDER_DISTANCE_SCORE_IMPACT =	20 --Border <-> province distance impact on province evaluation / actual distance penalty
@@ -24,3 +25,6 @@ NDefines.NAI.ESTATE_INTERACTION_THRESHOLD = 49.9
 NDefines.NAI.ESTATE_MAX_WANTED_INFLUENCE = 85.0
 NDefines.NAI.ESTATE_MIN_WANTED_CROWNLAND = 10.0
 NDefines.NAI.ESTATE_MAX_PRIVILEDGES = 4
+
+NDefines.NAI.PEACE_TERMS_PROVINCE_CORE_MULT = 10.0 -- AI desire for a province is multiplied by this if it has a core on it
+NDefines.NAI.PEACE_TERMS_PROVINCE_CLAIM_MULT = 7.5	--AI desire for a province is multiplied by this if it has a claim on it.
