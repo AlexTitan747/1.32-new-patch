8e5b92ef16f3e4409b8f829efe972cf966432f61
Sel-7 <confusedraven2003@gmail.com>
2023-01-09 02:14:59 +0000

    Have to pick decision for mana or FL since tribs are good now

--- common/defines/Titandefines.lua
+++ common/defines/Titandefines.lua
@@ -52,6 +52,13 @@ NDefines.NDiplomacy.HRE_VOTE_SAME_CULTURE_GROUP = -1
 NDefines.NDiplomacy.HRE_VOTE_ALLIANCE = 50
 NDefines.NDiplomacy.HRE_VOTE_ROYAL_MARRIAGE = 30
 NDefines.NDiplomacy.HRE_VOTE_CORE_CLAIM = 0
+NDefines.NDiplomacy.DAYS_TO_DECLARE_WAR = 0
+
+NDefines.NDiplomacy.TRIBUTE_BASE_CASH = 0.12
+NDefines.NDiplomacy.TRIBUTE_BASE_ADM = 0.12
+NDefines.NDiplomacy.TRIBUTE_BASE_DIP = 0.12
+NDefines.NDiplomacy.TRIBUTE_BASE_MIL = 0.12
+NDefines.NDiplomacy.TRIBUTE_BASE_MANPOWER = 0.1
 
 -- NCountry
 
