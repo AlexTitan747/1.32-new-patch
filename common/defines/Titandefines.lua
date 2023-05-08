a4615c4d5d5da5d122c1592271efbd8610018562
Tango1111 <69389299+Tango1111@users.noreply.github.com>
2023-05-08 14:59:13 +0100

    Actually fixing it

--- common/defines/Titandefines.lua
+++ common/defines/Titandefines.lua
@@ -129,9 +129,9 @@ NDefines.NEconomy.COT_DOWNGRADE_PRESTIGE_COST = 0
 
 -- NMilitary
 
-NDefines.NMilitary.SLACKEN_AP_DROP = 0.01,							--Defines how much AP is lost when activating slacken toggle.
-NDefines.NMilitary.SLACKEN_MIN_AP = 0,    							--Defines how much AP is at least needed to keep the Slacken Modifier active. Put a -1 here if you want the toggle to not turn off automatically
-NDefines.NMilitary.SLACKEN_MAX_MP_PERCENTAGE = 0.95,   			--Defines how much manpower percentage of your whole manpower pool you can have before the toggle turns off automatically. "1" would mean "100% of your manpower pool". Put a -1 here if you don't want this toggle to turn off from having manpower
+NDefines.NMilitary.SLACKEN_AP_DROP = 0.01							--Defines how much AP is lost when activating slacken toggle.
+NDefines.NMilitary.SLACKEN_MIN_AP = 0 							--Defines how much AP is at least needed to keep the Slacken Modifier active. Put a -1 here if you want the toggle to not turn off automatically
+NDefines.NMilitary.SLACKEN_MAX_MP_PERCENTAGE = 0.95   			--Defines how much manpower percentage of your whole manpower pool you can have before the toggle turns off automatically. "1" would mean "100% of your manpower pool". Put a -1 here if you don't want this toggle to turn off from having manpower
 
 NDefines.NMilitary.BANNER_AGE_MULTIPLIER = 0
 NDefines.NMilitary.BANNER_STARTING_STRENGTH = 0.1
