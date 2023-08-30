6d667773567f2aef72476fd4c82d8b20f77813ed
Tango1111 <69389299+Tango1111@users.noreply.github.com>
2023-08-30 19:09:13 +0100

    Slaken bugfix

--- common/defines/Titandefines.lua
+++ common/defines/Titandefines.lua
@@ -133,7 +133,7 @@ NDefines.NEconomy.USE_COLONIZER_CULTURE_ON_COLONY_COMPLETION_WITH_COLONIST = 1
 
 -- NMilitary
 
-NDefines.NMilitary.SLACKEN_AP_DROP = 0.01							--Defines how much AP is lost when activating slacken toggle.
+NDefines.NMilitary.SLACKEN_AP_DROP = 0.00						--Defines how much AP is lost when activating slacken toggle.
 NDefines.NMilitary.SLACKEN_MIN_AP = 0 							--Defines how much AP is at least needed to keep the Slacken Modifier active. Put a -1 here if you want the toggle to not turn off automatically
 NDefines.NMilitary.SLACKEN_MAX_MP_PERCENTAGE = 0.95   			--Defines how much manpower percentage of your whole manpower pool you can have before the toggle turns off automatically. "1" would mean "100% of your manpower pool". Put a -1 here if you don't want this toggle to turn off from having manpower
 
